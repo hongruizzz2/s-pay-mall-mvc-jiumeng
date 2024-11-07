bfd2e427d5b992500b39927000feba36d9b04aa7
hongrui <1169948065@qq.com>
2024-11-07 14:49:51 +0800

    init

---
+++ s-pay-mall-web/src/main/java/com/hongrui/controller/XxxController.java
@@ -0,0 +1,9 @@
+package com.hongrui.controller;
+
+/**
+ * @author hongrui
+ * @description
+ * @date 2024-11-07 0:20
+ */
+public class XxxController {
+}
---
+++ s-pay-mall-web/src/test/java/ApiTest.java
@@ -0,0 +1,7 @@
+/**
+ * @author hongrui
+ * @description
+ * @date 2024-11-07 0:18
+ */
+public class ApiTest {
+}
