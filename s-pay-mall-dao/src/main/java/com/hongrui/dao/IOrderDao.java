cfe1bba06eb8e122c8a1d0223529cafb1a2a880c
hongrui <1169948065@qq.com>
2024-11-07 20:50:46 +0800

    feat: 下单

---
+++ s-pay-mall-dao/src/main/java/com/hongrui/dao/IOrderDao.java
@@ -0,0 +1,16 @@
+package com.hongrui.dao;
+
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * @author hongrui
+ * @description
+ * @date 2024-11-07 20:47
+ */
+@Mapper
+public interface IOrderDao {
+
+    void insert();
+
+
+}
